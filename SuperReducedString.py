57d79b28a85f852ec567a226f5c1d8c84bc9de1a
Ernesto Alarcon <eazplus@gmail.com>
2016-09-01 01:17:49 -0500

    Solution in python for https://www.hackerrank.com/challenges/reduced-string

---
+++ SuperReducedString.py
@@ -0,0 +1,25 @@
+import sys
+
+def reduce_string (value):
+    result = []
+    i = 0
+    while i < len(value) - 1: 
+        if value[i] != value[i + 1]:
+            result.append(value[i])
+            i += 1
+        else:
+            i += 2
+    if i == len(value) - 1: result.append(value[-1])
+    return ''.join(result)
+
+value = sys.stdin.readlines()[0]
+value_len_new = -1
+value_len = 1
+
+while value_len != value_len_new:
+    value_len = len(value)
+    value = reduce_string (value)
+    value_len_new = len(value)
+
+if len(value) == 0: print('Empty String')
+else: print(value)
